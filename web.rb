457f64e0f1b0bab89d9dd8f7f51387b40fddcd5f
Alexander Mekelburg <alexmek@gmail.com>
2020-04-02 11:26:29 -0400

    add all error handling

--- web.rb
+++ web.rb
@@ -10,20 +10,37 @@ post '/SureOVRWebAPI/api/ovr' do
   
   @request_xml = request_payload["ApplicationData"]
   Post.create!(xml_request: @request_xml, query_string: params)
-  is_error = @request_xml =~ /<FirstName>ERROR<\/FirstName>/
+  is_error = @request_xml.to_s.downcase =~ /<firstname>error<\/firstname>/
   if is_error
-     @request_xml =~ /<LastName>(.+)<\/LastName>/
-     error_string = $1 || "There was an error"
-     "<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR>#{error_string}</ERROR></RESPONSE>"
-  else
-    is_invalid_penndot = (@request_xml =~ /<drivers-license>88888888<\/drivers-license>/) && (@request_xml =~ /<continueAppSubmit>0<\/continueAppSubmit>/)
-    if is_invalid_penndot
-      
-"<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR>VR_WAPI_InvalidOVRDL</ERROR></RESPONSE>"
-    else
-"<RESPONSE><APPLICATIONID>010101</APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR></ERROR></RESPONSE>"
-    end
+    raise "Error"
   end
+  
+  #<p>To trigger an empty application ID, use EMPTY as the first name.</p>
+  is_empty = @request_xml.to_s.downcase =~ /<firstname>empty<\/firstname>/
+  if is_empty
+    return "<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE></APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR></ERROR></RESPONSE>"
+  end
+  #<p>To trigger a malformed response, use FORMAT as the first name.</p>
+  is_bad_format = @request_xml.to_s.downcase =~ /<firstname>format<\/firstname>/
+  if is_bad_format
+    return "<RESPONSE>"
+  end
+  
+  #<p>To trigger an timeout response, use TIMEOUT as the first name.</p>
+  is_timeout = @request_xml.to_s.downcase =~ /<firstname>timeout<\/firstname>/
+  if is_timeout
+    sleep(130)
+    return "<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE></APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR></ERROR></RESPONSE>"
+  end
+  
+  
+  
+  is_specific_error = @request_xml =~ /<FirstName>(VR_WAPI_.+)<\/FirstName>/
+  if is_specific_error
+    error_string = $1 || "There was an error"
+    return "<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR>#{error_string}</ERROR></RESPONSE>"
+  end
+  return "<RESPONSE><APPLICATIONID>010101</APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR></ERROR></RESPONSE>"
 end
 
 
